be2e44e2b6ebaf53c01d2e4aa34d80d8d4439ed3
Krititya Onbun-ua <krittiya.on@mail.kmutt.ac.th>
2021-03-06 01:03:58 +0700

    [FIX] type google map

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -41,7 +32,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   // int _counter = 0;
-  
+
   Completer<GoogleMapController> _controller = Completer();
 
   static final CameraPosition _kGooglePlex = CameraPosition(
@@ -54,7 +45,6 @@ class _MyHomePageState extends State<MyHomePage> {
       target: LatLng(13.652720, 100.493635),
       tilt: 59.440717697143555,
       zoom: 19.151926040649414);
-
 
   // void _incrementCounter() {
   //   setState(() {
@@ -74,12 +64,12 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: GoogleMap(
-        mapType: MapType.hybrid,
+        mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
-          
+
         // child: Column(
         //   mainAxisAlignment: MainAxisAlignment.center,
         //   // children: <Widget>[
@@ -105,6 +95,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
   Future<void> _goToTheSIT() async {
     final GoogleMapController controller = await _controller.future;
     controller.animateCamera(CameraUpdate.newCameraPosition(_sit));

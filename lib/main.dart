95a5aad4cc952320733d5bc50cc231d58dc2c5ae
Krititya Onbun-ua <krittiya.on@mail.kmutt.ac.th>
2021-06-06 13:41:00 +0700

    [WIP] login & register
    There are some error

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+import 'package:cs_senior_project/asset/constant.dart';
+import 'package:cs_senior_project/notifiers/user_notifier.dart';
+import 'package:cs_senior_project/screens/home.dart';
+import 'package:cs_senior_project/screens/login.dart';
+import 'package:cs_senior_project/widgets/loading_widget.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -5,34 +10,66 @@ import 'package:provider/provider.dart';
 import './component/bottomBar.dart';
 import 'notifiers/store_notifier.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MultiProvider(
       providers: [
+        ChangeNotifierProvider.value(value: UserNotifier.initialize()),
         ChangeNotifierProvider(create: (context) => StoreNotifier()),
         // ChangeNotifierProvider(create: (context) => LocationNotifier()),
-        // ChangeNotifierProvider.value(value: UserNotifier.initialize())
-        // ChangeNotifierProvider(create: (context) => UserNotifier.initialize())
       ],
-      child: MyApp()));
+      child: MaterialApp(
+        theme: ThemeData(
+          primaryColor: Colors.white,
+          backgroundColor: Colors.white,
+        ),
+        home: MyApp(),
+        // initialRoute: '/',
+        // routes: {
+        //   bottomBar.routeName: (context) => bottomBar(),
+        // },
+      )));
 }
 
 class MyApp extends StatelessWidget {
-  static final String title = 'Home';
+  // static final String title = 'Home';
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.white,
-        backgroundColor: Colors.white,
-      ),
-      initialRoute: '/',
-      routes: {
-        bottomBar.routeName: (context) => bottomBar(),
-      },
-    );
+    UserNotifier auth = Provider.of<UserNotifier>(context);
+
+    return FutureBuilder(
+        future: initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Scaffold(
+                body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Text("Something went wrong")],
+            ));
+          }
+
+          if (snapshot.connectionState == ConnectionState.done) {
+            switch (auth.status) {
+              case Status.Uninitialized:
+                return LoadingWidget();
+              case Status.Unauthenticated:
+              case Status.Authenticating:
+                return LoginPage();
+              case Status.Authenticated:
+                return bottomBar();
+              default:
+                return LoginPage();
+            }
+          }
+
+          return Scaffold(
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [CircularProgressIndicator()],
+            ),
+          );
+        });
   }
 }
-

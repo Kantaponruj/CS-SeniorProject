cc4543f7ce32c9096aeca41efc46123d9b5bff62
Krititya Onbun-ua <krittiya.on@mail.kmutt.ac.th>
2021-06-11 21:15:46 +0700

    [ADD] mock up log out

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -2,10 +2,12 @@ import 'dart:async';
 
 import 'package:cs_senior_project/component/appBar.dart';
 import 'package:cs_senior_project/models/store.dart';
+import 'package:cs_senior_project/notifiers/user_notifier.dart';
+import 'package:cs_senior_project/screens/login.dart';
 import 'package:cs_senior_project/widgets/search_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
+import 'package:provider/provider.dart';
 
 class MenuPage extends StatefulWidget {
   static const routeName = '/menu';
@@ -19,15 +21,34 @@ class _MenuPageState extends State<MenuPage> {
 
   @override
   Widget build(BuildContext context) {
+    UserNotifier userNotifier = Provider.of<UserNotifier>(context);
 
     return Scaffold(
-      appBar: RoundedAppBar(appBarTitle: 'Menu',),
+      appBar: RoundedAppBar(
+        appBarTitle: 'Menu',
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text('This is menu page'),
             // SearchWidget(),
+            ElevatedButton(
+              child: Text(
+                'Log out',
+                style: TextStyle(fontSize: 15.0),
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+              ),
+              onPressed: () {
+                userNotifier.signOut();
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                    (route) => false);
+              },
+            ),
           ],
         ),
       ),

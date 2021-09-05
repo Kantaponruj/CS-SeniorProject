049c22aa8c005ad4dec6026d638392916631a2dd
Kantapon Rujijanakul <Kantapon.152543@mail.kmutt.ac.th>
2021-09-06 00:32:52 +0700

    ADD date&time display UI component

---
+++ lib/component/show_datetime.dart
@@ -0,0 +1,35 @@
+import 'package:cs_senior_project/asset/text_style.dart';
+import 'package:flutter/material.dart';
+
+class ShowDateTime extends StatelessWidget {
+  ShowDateTime({Key key,@required this.icon,@required this.text, @required this.onClicked}) : super(key: key);
+
+  final IconData icon;
+  final String text;
+  final VoidCallback onClicked;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Icon(icon),
+          Container(
+            margin: EdgeInsets.only(left: 20),
+            child: ElevatedButton(
+              onPressed: onClicked,
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                shadowColor: Colors.grey,
+              ),
+              child: Text(
+                text,
+                style: FontCollection.bodyTextStyle,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

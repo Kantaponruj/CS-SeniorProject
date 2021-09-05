d76d3d6fcf65c359df5e5435239f777d1f7dbf4d
Kantapon Rujijanakul <Kantapon.152543@mail.kmutt.ac.th>
2021-09-06 00:32:21 +0700

    ADD date&time picker widget

---
+++ lib/widgets/datetime_picker_widget.dart
@@ -0,0 +1,84 @@
+import 'package:cs_senior_project/component/show_datetime.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DatePickerWidget extends StatefulWidget {
+  DatePickerWidget({Key key}) : super(key: key);
+
+  @override
+  _DatePickerWidgetState createState() => _DatePickerWidgetState();
+}
+
+class _DatePickerWidgetState extends State<DatePickerWidget> {
+  DateTime date;
+
+  String getText() {
+    if (date == null) {
+      return 'Select Date';
+    } else {
+      return DateFormat('MM/dd/yyyy').format(date);
+      // return '${date.month}/${date.day}/${date.year}';
+    }
+  }
+
+  Future pickDate(BuildContext context) async {
+    final initialDate = DateTime.now();
+    final newDate = await showDatePicker(
+      context: context,
+      initialDate: date ?? initialDate,
+      firstDate: DateTime(DateTime.now().year - 5),
+      lastDate: DateTime(DateTime.now().year + 5),
+    );
+
+    if (newDate == null) return;
+
+    setState(() => date = newDate);
+  }
+
+  @override
+  Widget build(BuildContext context) => ShowDateTime(
+        icon: Icons.calendar_today,
+        text: getText(),
+        onClicked: () => pickDate(context),
+      );
+}
+
+class TimePickerWidget extends StatefulWidget {
+  @override
+  _TimePickerWidgetState createState() => _TimePickerWidgetState();
+}
+
+class _TimePickerWidgetState extends State<TimePickerWidget> {
+  TimeOfDay time;
+
+  String getText() {
+    if (time == null) {
+      return 'Select Time';
+    } else {
+      final hours = time.hour.toString().padLeft(2, '0');
+      final minutes = time.minute.toString().padLeft(2, '0');
+
+      return '$hours:$minutes';
+    }
+  }
+
+  Future pickTime(BuildContext context) async {
+    final initialTime = TimeOfDay(hour: 9, minute: 0);
+    final newTime = await showTimePicker(
+      context: context,
+      initialTime: time ?? initialTime,
+    );
+
+    if (newTime == null) return;
+
+    setState(() => time = newTime);
+  }
+
+  @override
+  Widget build(BuildContext context) => ShowDateTime(
+    icon: Icons.access_time,
+    text: getText(),
+    onClicked: () => pickTime(context),
+  );
+
+}

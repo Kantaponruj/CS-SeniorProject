1888a756be2a4819a43e5bf99a7d27cb5890a2a3
Krititya Onbun-ua <krittiya.on@mail.kmutt.ac.th>
2021-11-14 00:54:34 +0700

    [UPDATE] selected address

--- lib/screens/address/select_address.dart
+++ lib/screens/address/select_address.dart
@@ -1,13 +1,14 @@
 import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cs_senior_project/asset/constant.dart';
 import 'package:cs_senior_project/component/appBar.dart';
 import 'package:cs_senior_project/notifiers/location_notifer.dart';
 import 'package:cs_senior_project/notifiers/user_notifier.dart';
-import 'package:cs_senior_project/widgets/button_widget.dart';
 import 'package:cs_senior_project/widgets/loading_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_place_picker/google_maps_place_picker.dart';
 import 'package:provider/provider.dart';
 
 class SelectAddress extends StatefulWidget {
@@ -18,7 +19,7 @@ class SelectAddress extends StatefulWidget {
 }
 
 class _SelectAddressState extends State<SelectAddress> {
-  final Completer<GoogleMapController> _mapController = Completer();
+  // final Completer<GoogleMapController> _mapController = Completer();
 
   @override
   void initState() {
@@ -46,58 +47,84 @@ class _SelectAddressState extends State<SelectAddress> {
                 : Container(
                     child: Flexible(
                       flex: 15,
-                      child: GoogleMap(
-                        myLocationEnabled: true,
-                        mapType: MapType.normal,
-                        initialCameraPosition: CameraPosition(
-                          target: location.initialPosition,
-                          zoom: 18,
-                        ),
-                        markers: Set<Marker>.of(location.marker.values),
-                        onMapCreated: (GoogleMapController controller) {
-                          _mapController.complete(controller);
+                      child: PlacePicker(
+                        apiKey: GOOGLE_MAPS_API_KEY,
+                        initialPosition: location.initialPosition,
+                        useCurrentLocation: true,
+                        selectInitialPosition: true,
+                        usePlaceDetailSearch: true,
+                        onPlacePicked: (selectedPlace) {
+                          Navigator.of(context).pop();
+                          setState(() {
+                            userNotifier.updateUserData({
+                              'selectedAddress': {
+                                'address': selectedPlace.formattedAddress,
+                                'addressDetail': '',
+                                'addressName': '',
+                                'geoPoint': GeoPoint(
+                                    selectedPlace.geometry.location.lat,
+                                    selectedPlace.geometry.location.lng),
+                                'phone': userNotifier.userModel.phone,
+                                'residentName':
+                                    userNotifier.userModel.displayName,
+                              }
+                            });
+                            userNotifier.reloadUserModel();
+                          });
                         },
                       ),
+                      // GoogleMap(
+                      //   myLocationEnabled: true,
+                      //   mapType: MapType.normal,
+                      //   initialCameraPosition: CameraPosition(
+                      //     target: location.initialPosition,
+                      //     zoom: 18,
+                      //   ),
+                      //   markers: Set<Marker>.of(location.marker.values),
+                      //   onMapCreated: (GoogleMapController controller) {
+                      //     _mapController.complete(controller);
+                      //   },
+                      // ),
                     ),
                   ),
           ],
         ),
-        bottomNavigationBar: Container(
-          alignment: Alignment.bottomCenter,
-          height: 180,
-          margin: EdgeInsets.symmetric(horizontal: 20),
-          color: Colors.transparent,
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.fromLTRB(20, 30, 20, 10),
-                child: Container(
-                  child: Text(location.currentAddress != null
-                      ? location.currentAddress
-                      : 'ที่อยู่'),
-                ),
-              ),
-              StadiumButtonWidget(
-                text: 'เลือกตำแหน่ง',
-                onClicked: () {
-                  userNotifier.updateUserData({
-                    'selectedAddress': {
-                      'address': location.currentAddress,
-                      'addressDetail': '',
-                      'addressName': '',
-                      'geoPoint': GeoPoint(location.currentPosition.latitude,
-                          location.currentPosition.longitude),
-                      'phone': userNotifier.userModel.phone,
-                      'residentName': userNotifier.userModel.displayName,
-                    }
-                  });
-                  userNotifier.reloadUserModel();
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          ),
-        ),
+        // bottomNavigationBar: Container(
+        //   alignment: Alignment.bottomCenter,
+        //   height: 180,
+        //   margin: EdgeInsets.symmetric(horizontal: 20),
+        //   color: Colors.transparent,
+        //   child: Column(
+        //     children: [
+        //       Padding(
+        //         padding: EdgeInsets.fromLTRB(20, 30, 20, 10),
+        //         child: Container(
+        //           child: Text(location.currentAddress != null
+        //               ? location.currentAddress
+        //               : 'ที่อยู่'),
+        //         ),
+        //       ),
+        //       StadiumButtonWidget(
+        //         text: 'เลือกตำแหน่ง',
+        //         onClicked: () {
+        //           userNotifier.updateUserData({
+        //             'selectedAddress': {
+        //               'address': location.currentAddress,
+        //               'addressDetail': '',
+        //               'addressName': '',
+        //               'geoPoint': GeoPoint(location.currentPosition.latitude,
+        //                   location.currentPosition.longitude),
+        //               'phone': userNotifier.userModel.phone,
+        //               'residentName': userNotifier.userModel.displayName,
+        //             }
+        //           });
+        //           userNotifier.reloadUserModel();
+        //           Navigator.pop(context);
+        //         },
+        //       ),
+        //     ],
+        //   ),
+        // ),
       ),
     );
   }
